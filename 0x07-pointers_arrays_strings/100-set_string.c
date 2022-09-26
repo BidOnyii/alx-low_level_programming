baf8e9997a82595dccc1bc815a8622b86c0918c9
BidOnyii <98111387+BidOnyii@users.noreply.github.com>
2022-09-26 22:56:50 +0100

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,33 +1,29 @@
 #include "main.h"
 
-
-
 /**
+ * _strpbrk - searches a string for any of a set of bytes.
  *
- *  * set_string - sets the value of a pointer to a char
+ * @s: first string.
  *
- *   *
+ * @accept: second string.
  *
- *    * @s: source adress.
+ * Return: a pointer to the byte in s that matches one of the
  *
- *     *
+ * bytes in accept, or NULL if no such byte is found.
  *
- *      * @to: target adress.
- *
- *       *
- *
- *        * Return: no return.
- *
- *         *
- *
- *          */
+ */
 
-
-
-void set_string(char **s, char *to)
-
+char *_strpbrk(char *s, char *accept)
 {
+	unsigned int i, j;
 
-		*s = to;
-
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+				return (s + i);
+		}
+	}
+	return ('\0');
 }
