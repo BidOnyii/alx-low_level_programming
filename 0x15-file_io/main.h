a03205032391f0bc09c46ee197ca90f6731a44cc
BidOnyii <98111387+BidOnyii@users.noreply.github.com>
2022-11-01 00:15:59 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,41 +1,14 @@
-#include "main.h"
+#ifndef __MAIN_H__
+#define __MAIN_H__
 
-/**
- * read_textfile - reads a text file and prints it to standard output
- * @filename: name of the file
- * @letters: number of letters
- * Return: if success return number of letter, else return 0
- */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-	int fd;
-	char *buffer;
-	ssize_t bytes_read, bytes_written;
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
-	if (filename == NULL)
-		return (0);
-
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-
-	buffer = malloc(sizeof(char) * letters + 1);
-	if (buffer == NULL)
-		return (0);
-
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
-		return (0);
-
-	buffer[letters + 1] = '\0';
-	close(fd);
-
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1)
-		return (0);
-
-	free(buffer);
-
-	return (bytes_read);
-}
+#endif
